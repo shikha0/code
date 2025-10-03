0f8dce01c27a0a92634b93443092da0cb448251c
shikha0 <47059718+shikha0@users.noreply.github.com>
2025-10-03 20:59:33 +0530

    Update mainfile.py

--- Build/mainfile.py
+++ Build/mainfile.py
@@ -18,4 +18,4 @@ def get_user(username):
             return f"HTTP Error: {e}", e.response.status_code
 
 if __name__== "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port='8080')
